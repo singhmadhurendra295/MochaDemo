3c4bbc3ae74e8053a5a557f6b387659877a9205e
madhurendra <madhurendra@click-labs.com>
2018-03-09 22:57:20 +0530

    first commmit

---
+++ test/first_spec.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+describe('A feature', function(){
+	describe('A scenario', function(){
+		it('should respond to GET',function(){
+   			assert(true);
+  		 });
+	});
+});
