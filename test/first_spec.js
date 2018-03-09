ea6e6b44bf8203dd08b7bcfa965b08630dc4994d
madhurendra <madhurendra@click-labs.com>
2018-03-10 00:05:42 +0530

    basic test 1

--- test/first_spec.js
+++
@@ -1,8 +0,0 @@
-var assert = require("assert");
-describe('A feature', function(){
-	describe('A scenario', function(){
-		it('should respond to GET',function(){
-   			assert(true);
-  		 });
-	});
-});
